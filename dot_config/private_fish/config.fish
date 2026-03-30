3b790dbc3587b1aa98facc046940d6dd8fc88146
Drew DeVore <drew@devorcula.com>
2026-03-30 09:02:42 -0600

    Update .config/fish/config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -13,101 +13,102 @@ end
 
 # Configuration for interactive sessions
 if status is-interactive
-  # Theme
-  fish_config theme choose "Catppuccin Mocha"
+    # Theme
+    fish_config theme choose "Catppuccin Mocha"
 
-  # Completions
-  if test -d /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d
-    source /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/*.fish
-  end
+    # Completions
+    if test -d /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d
+        source /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/*.fish
+    end
 
-  # Applications
-  ## -- Atuin
-  if type -q atuin
-    eval "$(atuin init fish)"
-  end
+    # Applications
+    ## -- Atuin
+    if type -q atuin
+        eval "$(atuin init fish)"
+    end
 
-  ## -- Zoxide
-  if type -q zoxide
-    eval "$(zoxide init fish)"
-  end
+    ## -- Zoxide
+    if type -q zoxide
+        eval "$(zoxide init fish)"
+    end
 
-  ## -- Bat
-  if type -q bat
-    function cat
-      bat  $argv
+    ## -- Bat
+    if type -q bat
+        function cat
+            bat $argv
+        end
+        set -gx MANPAGER "bat -plman"
     end
-    set -gx MANPAGER "bat -plman"
-  end
 
-  ## -- cpx
-  if type -q cpx
-      function cp
-        cpx $argv
-      end
-  end
+    ## -- cpx
+    if type -q cpx
+        function cp
+            cpx $argv
+        end
+    end
 
-  ## -- eza
-  if type -q eza
-    function ls
-      eza $argv
+    ## -- eza
+    if type -q eza
+        function ls
+            eza $argv
+        end
+        function ll
+            eza -l --icons=auto --group-directories-first $argv
+        end
+        function l.
+            eza -d .*
+        end
+        function l1
+            eza -1 $argv
+        end
     end
-    function ll
-      eza -l --icons=auto --group-directories-first $argv
-    end
-    function l.
-      eza -d .*
-    end
-    function l1
-      eza -1 $argv
-    end
-  end
 
-  ## -- ugrep
-  if type -q ug
-    function grep
-      ug $argv
+    ## -- ugrep
+    if type -q ug
+        function grep
+            ug $argv
+        end
+        function egrep
+            ug -E $argv
+        end
+        function fgrep
+            ug -F $argv
+        end
+        function xzgrep
+            ug -z $argv
+        end
+        function xzegrep
+            ug -zE $argv
+        end
+        function xzfgrep
+            ug -zF $argv
+        end
     end
-    function egrep
-      ug -E $argv
-    end
-    function fgrep
-      ug -F $argv
-    end
-    function xzgrep
-      ug -z $argv
-    end
-    function xzegrep
-      ug -zE $argv
-    end
-    function xzfgrep
-      ug -zF $argv
-    end
-  end
 
-  ## -- zed flatpak
-  if test -d $HOME/.var/app/dev.zed.Zed
-    function zed
-      /usr/bin/flatpak run dev.zed.Zed $argv
+    ## -- zed flatpak
+    if test -d $HOME/.var/app/dev.zed.Zed
+        function zed
+            /usr/bin/flatpak run dev.zed.Zed $argv
+        end
     end
-  end
 
-  ## -- GNOME text editor flatpak
-  if test -d $HOME/.var/app/org.gnome.TextEditor
-    function gedit
-      /usr/bin/flatpak run org.gnome.TextEditor $argv
+    ## -- GNOME text editor flatpak
+    if test -d $HOME/.var/app/org.gnome.TextEditor
+        function gedit
+            /usr/bin/flatpak run org.gnome.TextEditor $argv
+        end
     end
-  end
 
-  ## -- VS Code
-  if test -d $HOME/.var/app/com.visualstudio.code
-    function code
-      /usr/bin/flatpak run com.visualstudio.code $argv
+    ## -- VS Code
+    if test -d $HOME/.var/app/com.visualstudio.code
+        function code
+            /usr/bin/flatpak run com.visualstudio.code $argv
+        end
     end
-  end
 end
 
 ### Starship prompt
-if type -q starship;
+if type -q starship
+
     starship init fish | source
 end
