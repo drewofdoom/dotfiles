76ffd06825c5f902671d794aeb8f4b2c858c0235
Drew DeVore <drew@devorcula.com>
2026-04-01 09:58:50 -0600

    Remove .config/fish/conf.d/proaudio.fish

--- dot_config/private_fish/conf.d/proaudio.fish
+++
@@ -1,5 +0,0 @@
-# proaudio distrobox container
-# if test -n "$CONTAINER_ID" -a "$CONTAINER_ID" = "proaudio"
-#     set -gx WINEPREFIX "/opt/wineprefixes/audio"
-#     set -gx WINEDEBUG "-all"
-# end
