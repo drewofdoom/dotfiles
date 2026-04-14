1550ddf918884c33f2a65707da080d8dc432353d
Drew DeVore <drew@devorcula.com>
2026-04-14 12:16:49 -0600

    Remove .bashrc.d/10-ble.sh Remove .bashrc.d/11-atuin.sh Remove .bashrc.d/50-cpx.sh Remove .bashrc.d/51-bat.sh Remove .bashrc.d/90-zed.sh Remove .bashrc.d/99-starship.sh

--- dot_bashrc.d/11-atuin.sh
+++
@@ -1,9 +0,0 @@
-# .bashrc.d/11-atuin.sh
-
-# Synced history replacement
-# Does not rely on BLE, but is enhanced with its readline functions
-
-# atuin - `brew install atuin`
-if [ "$(command -v atuin)" ]; then
-    eval "$(atuin init bash)"
-fi
